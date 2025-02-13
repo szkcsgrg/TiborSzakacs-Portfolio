a5c7667ea5d21d9b8a1c2ac44882fca65eea59d4
Gergő Szakács <szkcsgrg@gmail.com>
2025-02-13 12:54:20 +0100

    Updated the images.
    Still working on the google drive as Image storage solution. Until that this was the easiest and quick solution.

--- client/src/components/imports/tetovalasokPaths.ts
+++ client/src/components/imports/tetovalasokPaths.ts
@@ -6,7 +6,37 @@ import img4 from "../../assets/tetovalasok/IMG-bf8d44650366eb9f436e6e15415729f6-
 import img5 from "../../assets/tetovalasok/IMG-20240425-WA0000.jpg";
 import img6 from "../../assets/tetovalasok/IMG-20240825-WA0005~2.jpg";
 import img7 from "../../assets/tetovalasok/Messenger_creation_11b7bb35-0bf4-4e21-a888-fc8d7da0a7ba.jpg";
+import img8 from "../../assets/tetovalasok/img (1).jpeg";
+import img9 from "../../assets/tetovalasok/img (2).jpeg";
+import img10 from "../../assets/tetovalasok/img (3).jpeg";
+import img11 from "../../assets/tetovalasok/img (4).jpeg";
+import img12 from "../../assets/tetovalasok/img (5).jpeg";
+import img13 from "../../assets/tetovalasok/img (6).jpeg";
+import img14 from "../../assets/tetovalasok/img (7).jpeg";
+import img15 from "../../assets/tetovalasok/img (8).jpeg";
+import img16 from "../../assets/tetovalasok/img (9).jpeg";
+import img17 from "../../assets/tetovalasok/img (10).jpeg";
+import img18 from "../../assets/tetovalasok/img (11).jpeg";
+import img19 from "../../assets/tetovalasok/img (12).jpeg";
+import img20 from "../../assets/tetovalasok/img (13).jpeg";
+import img21 from "../../assets/tetovalasok/img (14).jpeg";
+import img22 from "../../assets/tetovalasok/img (15).jpeg";
+import img23 from "../../assets/tetovalasok/img (16).jpeg";
+import img24 from "../../assets/tetovalasok/img (17).jpeg";
+import img25 from "../../assets/tetovalasok/img (18).jpeg";
+import img26 from "../../assets/tetovalasok/img (19).jpeg";
+import img27 from "../../assets/tetovalasok/img (20).jpeg";
+import img28 from "../../assets/tetovalasok/img (21).jpeg";
+import img29 from "../../assets/tetovalasok/img (22).jpeg";
+import img30 from "../../assets/tetovalasok/img (23).jpeg";
+import img31 from "../../assets/tetovalasok/img (24).jpeg";
+import img32 from "../../assets/tetovalasok/img (25).jpeg";
+import img33 from "../../assets/tetovalasok/img (26).jpeg";
+import img34 from "../../assets/tetovalasok/img (27).jpeg";
+import img35 from "../../assets/tetovalasok/img (28).jpeg";
+import img36 from "../../assets/tetovalasok/img (29).jpeg";
+
 
 export const tetovalasokPaths = [
-    img1, img2, img3, img4, img5, img6, img7
+    img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26, img27, img28, img29, img30, img31, img32, img33, img34, img35, img36
 ];
